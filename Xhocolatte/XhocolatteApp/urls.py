845d69276e03f8c7b2c894e2cdd5e52c70087779
Dapapu7 <dapapu7@gmail.com>
2022-02-14 17:52:29 +0100

    Empezando Xhocolatte

---
+++ Xhocolatte/XhocolatteApp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from XhocolatteApp import views
+
+urlpatterns = [
+    path('', views.home, name = "Home"),
+    path('productos/', views.productos, name = "Productos"),
+    path('nosotros/', views.nosotros, name = "Nosotros"),
+    path('faq/', views.faq, name = "FAQ"),
+    path('contacto/', views.contacto, name = "Contacto"),
+    path('blog/', views.blog, name = "Blog"),
+] 
---
+++ Xhocolatte/XhocolatteApp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def  home(request):
+    return render(request, 'XhocolatteApp/home.html')
+
+def  productos(request):
+    return render(request, 'XhocolatteApp/productos.html')
+
+def  nosotros(request):
+    return render(request, 'XhocolatteApp/nosotros.html')
+
+def  faq(request):
+    return render(request, 'XhocolatteApp/faq.html')
+
+def  contacto(request):
+    return render(request, 'XhocolatteApp/contacto.html')
+
+def blog(request):
+    return render(request, 'XhocolatteApp/blog.html')
+
