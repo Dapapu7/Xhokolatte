7c01f20a82c6bd4f01d6d0c24c2d156cd559806d
Dapapu7 <dapapu7@gmail.com>
2022-05-19 14:53:13 +0200

    Password-Reset

--- Xhocolatte/account/urls.py
+++ Xhocolatte/account/urls.py
@@ -19,10 +19,10 @@ urlpatterns = [
     path('profile/delete_confirm/', TemplateView.as_view(template_name="account/dashboard/delete_confirm.html"), name='delete_confirmation'),
     
     # Password
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="account/registration/password_reset_form.html", success_url='password_reset_email_confirm', email_template_name='account/registration/password_reset_email.html', form_class=PwdResetForm), name='pwdreset'),
-    path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='account/registration/password_reset_confirm.html', success_url='/accounts/password_reset_complete/',  form_class=PwdResetConfirmForm), name="password_reset_confirm"),
-    path('password_reset/password_reset_email_confirm/', TemplateView.as_view(template_name="account/registration/reset_status.html"), name='password_reset_done'),
-    path('password_reset_complete/', TemplateView.as_view(template_name="account/registration/reset_status.html"), name='password_reset_complete'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="account/password_reset/password_reset_form.html", success_url='password_reset_email_confirm', email_template_name='account/password_reset/password_reset_email.html', form_class=PwdResetForm), name='pwdreset'),
+    path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='account/password_reset/password_reset_confirm.html', success_url='/accounts/password_reset_complete/',  form_class=PwdResetConfirmForm), name="password_reset_confirm"),
+    path('password_reset/password_reset_email_confirm/', TemplateView.as_view(template_name="account/password_reset/reset_status.html"), name='password_reset_done'),
+    path('password_reset_complete/', TemplateView.as_view(template_name="account/password_reset/reset_status.html"), name='password_reset_complete'),
 
     # Orders
     path('user_orders/', views.user_orders, name="user_orders"),
